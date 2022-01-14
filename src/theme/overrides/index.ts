b6fa1101b9b0b60044b19869e99fdafcf1a8964e
Timothy Mugo <timomugo01@gmail.com>
2022-01-14 13:51:07 +0300

    react typescript admin

---
+++ src/theme/overrides/index.ts
@@ -0,0 +1,29 @@
+import { merge } from 'lodash';
+import Card from './Card';
+import Lists from './Lists';
+import Paper from './Paper';
+import Input from './Input';
+import Button from './Button';
+import Tooltip from './Tooltip';
+import Backdrop from './Backdrop';
+import Typography from './Typography';
+import IconButton from './IconButton';
+import Autocomplete from './Autocomplete';
+
+// ----------------------------------------------------------------------
+
+export default function ComponentsOverrides (theme: any) {
+  return merge(
+    Card(theme),
+    Lists(theme),
+    // @ts-ignore
+    Paper(theme),
+    Input(theme),
+    Button(theme),
+    Tooltip(theme),
+    Backdrop(theme),
+    Typography(theme),
+    IconButton(theme),
+    Autocomplete(theme)
+  );
+}
---
+++ src/utils/mockImages.ts
@@ -0,0 +1,5 @@
+// ----------------------------------------------------------------------
+
+export const mockImgCover = (index: any) => `/static/mock-images/covers/cover_${index}.jpg`;
+export const mockImgProduct = (index: any) => `/static/mock-images/products/product_${index}.jpg`;
+export const mockImgAvatar = (index: any) => `/static/mock-images/avatars/avatar_${index}.jpg`;
